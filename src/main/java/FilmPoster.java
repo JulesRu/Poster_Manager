7c7294b58c40e81eb6d3b9c3ce60094b08ccd0f5
JuliaR <donidonic@gmail.com>
2023-02-06 18:36:08 +0300

    Methods and tests added

---
+++ src/main/java/FilmPoster.java
@@ -0,0 +1,10 @@
+public class FilmPoster {
+    private int id;
+    private String filmName;
+
+
+    public FilmPoster(int id, String filmName) {
+        this.id = id;
+        this.filmName = filmName;
+    }
+}
---
+++ src/test/java/PosterManagerTest.java
@@ -0,0 +1,153 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class PosterManagerTest {
+    FilmPoster poster1 = new FilmPoster(23, "Бладшот");
+    FilmPoster poster2 = new FilmPoster(14, "Вперёд");
+    FilmPoster poster3 = new FilmPoster(4, "Отель 'Белград'");
+    FilmPoster poster4 = new FilmPoster(1, "Гарри Поттер и философский камень");
+    FilmPoster poster5 = new FilmPoster(2, "Гарри Поттер и тайная комната");
+    FilmPoster poster6 = new FilmPoster(3, "Гарри Поттер и узник Азкабана");
+    FilmPoster poster7 = new FilmPoster(4, "Гарри Поттер и кубок огня");
+    FilmPoster poster8 = new FilmPoster(5, "Гарри Поттер и орден Феникса");
+    FilmPoster poster9 = new FilmPoster(6, "Гарри Поттер и принц-полукровка");
+    FilmPoster poster10 = new FilmPoster(7, "Гарри Поттер и дары смерти");
+    FilmPoster poster11 = new FilmPoster(8, "Гарри Поттер и проклятое дитя");
+    FilmPoster poster12 = new FilmPoster(9, "Фантастические твари и где они обитают");
+    FilmPoster poster13 = new FilmPoster(10, "Чародеи");
+    FilmPoster poster14 = new FilmPoster(11, "Я предпочитаю рай");
+    FilmPoster poster15 = new FilmPoster(12, "Безумные соседи");
+
+    /*@BeforeEach
+    public void setup() {
+        poster.save(poster1);
+        poster.save(poster2);
+        poster.save(poster3);
+        poster.save(poster4);
+        poster.save(poster5);
+        poster.save(poster6);
+        poster.save(poster7);
+        poster.save(poster8);
+        poster.save(poster9);
+        poster.save(poster10);
+        poster.save(poster11);
+        poster.save(poster12);
+        poster.save(poster13);
+        poster.save(poster14);
+        poster.save(poster15);
+    }*/
+
+
+    @Test
+    public void saveNewPosters() {
+
+        PosterManager poster = new PosterManager();
+
+        poster.save(poster1);
+        poster.save(poster2);
+        poster.save(poster3);
+
+        FilmPoster[] expected = {poster1, poster2, poster3};
+        FilmPoster[] actual = poster.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+
+    @Test
+    public void reversedByDefaultLimit() {
+
+        PosterManager poster = new PosterManager();
+
+        poster.save(poster1);
+        poster.save(poster2);
+        poster.save(poster3);
+        poster.save(poster4);
+        poster.save(poster5);
+        poster.save(poster6);
+        poster.save(poster7);
+        poster.save(poster8);
+        poster.save(poster9);
+        poster.save(poster10);
+        poster.save(poster11);
+        poster.save(poster12);
+        poster.save(poster13);
+        poster.save(poster14);
+        poster.save(poster15);
+
+        FilmPoster[] expected = {poster15, poster14, poster13, poster12, poster11, poster10, poster9, poster8, poster7, poster6};
+        FilmPoster[] actual = poster.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+
+    @Test
+    public void reversedByDefaultUnderLimit() {
+
+        PosterManager poster = new PosterManager();
+
+        poster.save(poster1);
+        poster.save(poster2);
+        poster.save(poster3);
+        poster.save(poster4);
+        poster.save(poster5);
+        poster.save(poster6);
+        poster.save(poster7);
+        poster.save(poster8);
+
+        FilmPoster[] expected = {poster8, poster7, poster6, poster5, poster4, poster3, poster2, poster1};
+        FilmPoster[] actual = poster.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+
+
+    }
+
+    @Test
+    public void reversedRandomLimit() {
+
+        PosterManager poster = new PosterManager(4);
+
+        poster.save(poster1);
+        poster.save(poster2);
+        poster.save(poster3);
+        poster.save(poster4);
+        poster.save(poster5);
+        poster.save(poster6);
+        poster.save(poster7);
+        poster.save(poster8);
+        poster.save(poster9);
+        poster.save(poster10);
+        poster.save(poster11);
+        poster.save(poster12);
+        poster.save(poster13);
+        poster.save(poster14);
+        poster.save(poster15);
+
+        FilmPoster[] expected = {poster15, poster14, poster13, poster12};
+        FilmPoster[] actual = poster.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void reversedUnderRandomLimit() {
+
+        PosterManager poster = new PosterManager(4);
+
+        poster.save(poster1);
+        poster.save(poster2);
+        poster.save(poster3);
+
+
+        FilmPoster[] expected = {poster3, poster2, poster1};
+        FilmPoster[] actual = poster.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+
+}
